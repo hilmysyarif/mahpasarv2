6083bd80d5147811b855859799d9a318925404fd
Hilmy Syarif <hilmysyarif@gmail.com>
2020-05-30 23:14:55 +0700

    added penjual to default registered user

--- app/User.php
+++ app/User.php
@@ -12,7 +12,6 @@ class User extends Authenticatable
     use Notifiable;
     use HasRoles;
     
-    protected $guard_name = 'api';
     /**
      * The attributes that are mass assignable.
      *
