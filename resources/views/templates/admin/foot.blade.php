7e7caf1faebe44b71f60e6a20436d7db6821d77c
Hilmy Syarif <hilmysyarif@gmail.com>
2020-05-30 20:45:36 +0700

    footer admin

--- resources/views/templates/admin/foot.blade.php
+++ resources/views/templates/admin/foot.blade.php
@@ -3,7 +3,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2020 <a href="">Ahmad Kurniawan</a>.</strong> All rights
+    <strong>Copyright &copy; 2020 <a href="{{ url('/') }}">{{ config('app.name') }}</a>.</strong> All rights
     reserved.
   </footer>
   <div class="control-sidebar-bg"></div>
