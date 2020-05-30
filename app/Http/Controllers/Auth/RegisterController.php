1984b9dfacbda2a98442924a3fd7d4c876fe1de7
Purwanto <purwanto@gmail.com>
2020-05-30 18:51:51 +0700

    tambahin package role

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
 {
@@ -64,11 +65,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'role' => 2, //customer
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->assignRole('member');
+        
+        return $user;
     }
 }
